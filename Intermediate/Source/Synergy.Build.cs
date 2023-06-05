21fb5fff58e81288afb906182335377d3d118b59
Tevtongermany <alexm3350@gmail.com>
2023-06-05 17:40:03 +0200

    sourcecode

---
+++ Intermediate/Source/Synergy.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Synergy : ModuleRules
+{
+	public Synergy(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
